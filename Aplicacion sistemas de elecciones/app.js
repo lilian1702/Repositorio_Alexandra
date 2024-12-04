e2209819fdf25cd901378e0cedb9120401d3e5ac
lilian1702 <163610265+lilian1702@users.noreply.github.com>
2024-12-03 21:14:26 -0500

    cambios en la app

--- Aplicacion sistemas de elecciones/app.js
+++ Aplicacion sistemas de elecciones/app.js
@@ -1,12 +1,12 @@
 let votos = [0, 0, 0, 0]; 
 document.addEventListener("DOMContentLoaded", () => {
-   // Array de votos inicializado
+  
   let div = document.querySelector("#div");
   const cerrarBtn = document.querySelector("#cerrar");
   const resultado = document.querySelector("#resultado");
   const lista = document.querySelector("#resultados");
 
-  // Botón de cerrar sesión
+  
   if (cerrarBtn) {
     cerrarBtn.addEventListener("click", () => {
       alert("Sesión cerrada");
@@ -14,7 +14,7 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   }
 
-  // Mostrar resultados
+ 
   if (resultado && lista) {
     resultado.addEventListener("click", () => {
       lista.innerHTML = `
@@ -33,12 +33,12 @@ document.addEventListener("DOMContentLoaded", () => {
 
 let div = document.querySelector("#div");
 
-// Componente de consumo API, mostrando todos los archivos
+
 fetch("https://raw.githubusercontent.com/cesarmcuellar/Elecciones/refs/heads/main/candidatos.json")
   .then(response => response.json())
   .then(candidatos => {
     candidatos.forEach((candidato, index) => {
-      // Inserta los datos de los candidatos en la interfaz
+     
       div.innerHTML += `
         <div class="card">
             <h3>${candidato.curso}</h3>
@@ -49,30 +49,30 @@ fetch("https://raw.githubusercontent.com/cesarmcuellar/Elecciones/refs/heads/mai
       `;
     });
 
-    // Asignamos el evento de clic a las imágenes
+  
     let img = document.querySelectorAll('.fotos');
 
     img.forEach(imagen => {
       imagen.addEventListener('click', () => {
-        let index = imagen.getAttribute('data-index');  // Obtiene el índice del candidato
+        let index = imagen.getAttribute('data-index');  
         let nombre= imagen.getAttribute('data-nombre');
 
         let confirmacion = confirm(`¿Deseas votar por ${nombre}?`);
 
         if (confirmacion) {
-          // Incrementa el voto del candidato según el índice
+          
           votos[index]++;
           alert(`¡Tu voto ha sido registrado para el candidato ${nombre}!`);
         }
 
-        console.log(votos);  // Muestra los votos en consola para ver si se registran correctamente
+        console.log(votos);  
       });
     });
   });
 
 
 
-//Login cuando te registras y todo sale bien te deja ingresar al portal para que votes
+
 document.querySelector("#btn").addEventListener("click",()=>{
 let user = document.querySelector("#usuario").value;
 let contra = document.querySelector("#contrasena").value;
